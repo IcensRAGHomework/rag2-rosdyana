08a7789ec848d2d77fb3256182c30ef22b61f9fc
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2025-01-23 12:08:21 +0800

    [fix] add implementation hw02_1

--- student_assignment.py
+++ student_assignment.py
@@ -1,13 +1,31 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import (CharacterTextSplitter,
-                                      RecursiveCharacterTextSplitter)
+from langchain_text_splitters import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
 
 q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    # Load PDF using PyPDFLoader
+    loader = PyPDFLoader(q1_pdf)
+    documents = loader.load()
+
+    # Initialize CharacterTextSplitter with chunk_overlap=0
+    text_splitter = CharacterTextSplitter(chunk_overlap=0)
+
+    # Split PDF by page
+    split_documents = text_splitter.split_documents(documents)
+
+    return split_documents[-1]
+
 
 def hw02_2(q2_pdf):
     pass
+
+
+# Testing
+print(hw02_1(q1_pdf))
+# print(hw02_2(q2_pdf))
